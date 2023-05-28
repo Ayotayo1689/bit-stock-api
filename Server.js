e5936a507c394887fd89cb5072293f06ecf7dcbe
ayotayo <ayotayobadejo@gmail.com>
2023-05-28 14:56:01 +0100

    first api  commit

--- Server.js
+++ Server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 
 
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
     res.status(201).json({
         message: 'welcome to bitstock api '
     })
